89aebd90310c07a413ac3932eb9aa243f72a318c
Chris Beaver <chrisbvr@gmail.com>
2017-07-10 20:47:38 +1000

    Simple Title Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'; 
 
+import Title from './Title';
+
 class App extends Component {
   render() {
     return (
       <div>
-        This is the app 
+        <Title /> 
       </div>
     )
   }
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chris's Blog</h1>
+        <h4>One does wonder if it's real</h4>
+      </div>
+    )
+  }
+}
+
+export default Title; 
